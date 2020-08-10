2b09a33170e559937d03c7d3956759410c41925a
Traphume <traphumedev@gmail.com>
2020-08-10 12:28:08 +0700

    Revert "Added helper constructor to UserService"
    This reverts commit 65e27942c58f96ae7d5abc3b2e2cd633d77ccf6c.

--- db/user.go
+++ db/user.go
@@ -1,30 +1,13 @@
 package db
 
 import (
-	"context"
 	"github.com/jmoiron/sqlx"
 	"github.com/tphume/converse-be"
-	"time"
 )
 
 // Represent interactions to the user table in the database
 type UserService struct {
 	DB *sqlx.DB
-}
-
-// Helper constructor method
-func NewUserService(driver string, dataSource string) (*UserService, error) {
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second*30)
-	defer cancel()
-
-	conn, err := sqlx.ConnectContext(ctx, driver, dataSource)
-	if err != nil {
-		return nil, err
-	}
-
-	return &UserService{
-		DB: conn,
-	}, nil
 }
 
 // Basic CRUD methods
