01c170be313c0555184648dd0f9fce6a4b1eaf98
Traphume <traphumedev@gmail.com>
2020-08-10 12:29:19 +0700

    Added helper function to get db connection

--- db/db.go
+++ db/db.go
@@ -1,6 +1,25 @@
 package db
 
+import (
+	"context"
+	"github.com/jmoiron/sqlx"
+	"time"
+)
+
 // Client representing the services in the system
 type Client struct {
 	User UserService
+}
+
+// Helper function to return SQL connection
+func GetSQLConn(driver string, dataSource string) (*sqlx.DB, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*30)
+	defer cancel()
+
+	conn, err := sqlx.ConnectContext(ctx, driver, dataSource)
+	if err != nil {
+		return nil, err
+	}
+
+	return conn, nil
 }
