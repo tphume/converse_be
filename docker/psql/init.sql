62b4cdd17e2e8bcbd47196d224b7d69868c3c38f
Traphume <traphumedev@gmail.com>
2020-08-14 15:53:17 +0700

    Updated init sql script

--- docker/psql/init.sql
+++ docker/psql/init.sql
@@ -1,6 +1,9 @@
 CREATE TABLE users
 (
-    id       VARCHAR(36),
-    name     VARCHAR(20),
-    password CHAR(60)
-)
+    id       CHAR(36)    NOT NULL,
+    name     VARCHAR(20) NOT NULL UNIQUE,
+    password CHAR(60)    NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE UNIQUE INDEX name ON users (name);
