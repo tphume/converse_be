4d9304c5e80f70a582c4c912a56ed8f2fe02ae5a
Traphume <traphumedev@gmail.com>
2020-08-14 15:54:48 +0700

    Implemented Basic Create User operation

--- db/user.go
+++ db/user.go
@@ -1,8 +1,10 @@
 package db
 
 import (
+	"context"
 	"github.com/jmoiron/sqlx"
 	"github.com/tphume/converse_be"
+	"time"
 )
 
 // Represent interactions to the user table in the database
@@ -12,7 +14,15 @@ type UserService struct {
 
 // Basic CRUD methods
 func (s *UserService) CreateUser(user *converse_be.User) error {
-	panic("implement me")
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*25)
+	defer cancel()
+
+	_, err := s.DB.NamedExecContext(ctx, sqlCreate, user)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func (s *UserService) ReadUser(user *converse_be.User) error {
@@ -26,3 +36,8 @@ func (s *UserService) UpdateUser(user *converse_be.User) error {
 func (s *UserService) DeleteUser(name string, id string) error {
 	panic("implement me")
 }
+
+// Exec and Query strings
+const (
+	sqlCreate = `INSERT INTO users (id, name, password) VALUES (:id, :name, :password)`
+)
