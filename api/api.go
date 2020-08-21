48b4f5becad58b4778184998d1e3c9db3da9c37a
Traphume <traphumedev@gmail.com>
2020-08-21 14:41:03 +0700

    Fixed function definition for server. Added run method to start api server

--- api/api.go
+++ api/api.go
@@ -2,6 +2,7 @@ package api
 
 import (
 	"github.com/gin-gonic/gin"
+	"log"
 )
 
 // Interface for our handler routes
@@ -11,17 +12,22 @@ type Handler interface {
 
 // Our http web service
 type Server struct {
+	Address  string
 	Engine   *gin.Engine
 	Handlers []Handler
 }
 
-func InitServer(s *Server) {
+func (s *Server) InitServer() {
 	router := s.Engine.Group("/api")
 	for _, h := range s.Handlers {
 		if err := h.InitRoute(router); err != nil {
 			panic(err)
 		}
 	}
+}
+
+func (s *Server) RunServer() {
+	log.Fatal(s.Engine.Run(s.Address))
 }
 
 // Error messages
